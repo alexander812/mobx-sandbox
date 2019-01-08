f4ec123ab6c99f327c03ee27ee570726650c9bf4
Alexander Smirnov <a.smirnov@space307.com>
2019-01-08 20:34:42 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/index.jsx';
 
-
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(React.createElement(App), document.querySelector('#root'));
 });
